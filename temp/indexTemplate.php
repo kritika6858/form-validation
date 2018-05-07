7b5ececddbfe8cb3152a8ce13492816937cc2d47
kritika6858 <37039125+kritika6858@users.noreply.github.com>
2018-05-07 11:14:56 +0530

    Add files via upload

---
+++ temp/indexTemplate.php
@@ -0,0 +1,168 @@
+﻿<html>
+<head>
+	<title>Collage Name</title>
+<?php 
+include('connect.php');
+?>
+<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="bootstrap\css\bootstrap.css">
+<link rel="stylesheet" href="bootstrap\css\bootstrap.min.css">
+<link rel="stylesheet" href="bootstrap\css\bootstrap-grid.css">
+<link rel="stylesheet" href="bootstrap\css\bootstrap-grid.min.css">
+<link rel="stylesheet" href="bootstrap\css\bootstrap-reboot.css">
+<link rel="stylesheet" href="bootstrap\css\bootstrap-reboot.min.css">
+</head>
+<body id="back" style="margin-left: auto;margin-right: auto;max-width: 1366px;>
+	<a name="home"></a>
+	<div id=divbox style="width: 1340px;"><img src="images\logo.png" id="center">
+<!--####################################################Hyperlinks Below Logo####################################################-->
+	<div id=textcentre>
+		<div id="divboxnb" style="line-height: 43px;">
+		<a href="#home" id="ahref">Home</a>
+		<a href="#Gallery" id="ahref">Gallery</a>
+		<a href="#collegemap" id="ahref">College Map</a>
+		<a href="#Alumni" id="ahref">Alumni</a>
+		<a href="#about" id="ahref">About Us</a>
+		<a href="#contactus" id="ahref">Contact Us</a>
+	</div>
+	</div>
+	</div>
+<!--####################################################Homepage####################################################-->
+<div id="divbox" style="height: 415px;width: 1340px;">
+<!--####################################################Form Content####################################################-->
+<div id="divboxnb" style="float: right;height: 397px;width: 1115px;margin-top: 8px">
+
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
+<!--####################################################Dropdown and Quicklinks####################################################-->
+<div style="display: block;float: left;">
+	<div id="divboxnb" style="height: 250px;margin-left: 8px;margin-top: 8px;">
+		<div id="divMenu" style="line-height: 43px;">
+	    	<ul>
+	        	<li><a href="#" style="line-height: 35px;">Registration</a>
+	                <ul>
+			    		<li><a href="#">Student Registration</a></li>
+			    		<li><a href="#">Student Display</a></li>
+			    		<li><a href="#">Enter Marks</a></li>
+			    		<li><a href="#">Show Marks</a></li>
+			    		<li><a href="#">Library Staff</a></li>
+	        		</ul>
+	        	</li>
+	        	<li><a href="#" style="line-height: 35px;">Staff</a>
+	                <ul>
+			            <li><a href="#">Principal</a></li>
+			    		<li><a href="#">Computer Engineering</a></li>
+			    		<li><a href="#">Civil Engineering</a></li>
+			    		<li><a href="#">Mechanical Engineering</a></li>
+			    		<li><a href="#">Electrical Engineering</a></li>
+			    		<li><a href="#">Automobile Engineering</a></li>
+			    		<li><a href="#">Applied Science</a></li>
+			    		<li><a href="#">Sports Department</a></li>
+			    		<li><a href="#">Workshop Staff</a></li>
+			    		<li><a href="#">Official Staff</a></li>
+			    		<li><a href="#">Library Staff</a></li>
+	        		</ul>
+	        	</li>
+	        	<li><a href="#" style="line-height: 35px;">Administration</a>
+	                <ul>
+			            <li><a href="#">Collage Administration</a></li>
+			    		<li><a href="#">Enquiry Committies</a></li>
+			    		<li><a href="#">Managing Committies</a></li>
+			    		<li><a href="#">Teaching Committies</a></li>
+			    		<li><a href="#">Non-Teaching Committies</a></li>
+			    		<li><a href="#">Fianace Committies</a></li>
+			    		<li><a href="#">Purchase Committies</a></li>
+			    		<li><a href="#">CDRT Committies</a></li>
+	        		</ul>
+	        	</li>
+	        	<li><a href="#" style="line-height: 35px;">Departments</a>
+	                <ul>
+			            <li><a href="#">Computer Engineering</a></li>
+			    		<li><a href="#">Civil Engineering</a></li>
+			    		<li><a href="#">Mechanical Engineering</a></li>
+			    		<li><a href="#">Electrical Engineering</a></li>
+			    		<li><a href="#">Automobile Engineering</a></li>
+	        		</ul>
+	        	</li>
+	        	<li><a href="#" style="line-height: 35px;">Facilities</a>
+	                <ul>
+	            <li><a href="#">Hostel</a></li>
+	        		</ul>
+	        	</li>
+	        	<li><a href="#" style="line-height: 35px;">Training & Placement</a>
+	                <ul>
+	    		<li><a href="#">Training & Placement Cell</a></li>
+	   		 	<li><a href="#">Placement</a></li>
+	        		</ul>
+	        	</li>
+	        	<li><a href="#" style="line-height: 35px;">Anti-Ragging</a>
+	        	</li>
+	        </ul>
+	    </div>
+	</div>
+	<div id="divboxnb" style="height: 142px;margin-left: 8px;margin-top: 8px;">
+		<div id="divMenu" style="line-height: 43px;">
+		    <ul>
+		        <a id="pidforah" style="line-height: 35px;padding: 0px;background-color: rgba(84,153,199,0.3);"><font size="4"><b>Quick Links</b></a></font>
+		        <li><a href="http://results.pbteched.net/index.html" style="line-height: 35px;">Result</a></li>
+		        <li><a href="#" style="line-height: 35px;">Complaint Cell</a></li>
+		        <li><a href="#" style="line-height: 35px;">Scholarship</a></li>
+		    </ul>           
+    	</div>
+	</div>
+</div>
+</div>
+
+<!--####################################################Bottom Footer########################-->
+<div style="width: 1348px">
+	<div id="divbox" style="color: white;height: 240px">
+		<div style="width: 410px;float: left;margin-left: 40px;padding-top: 20px;">
+			<font size="5"><b>ABOUT US</b><br></font>
+			College is about 5 km. from bus Stand. Guru Nanak Dev Polytechnic College, Ludhiana is a State Government Aided Minority Institution.It is functioning under the aegis of Nankana Sahib Education Trust with a Managing Committee to look after routine management functions with due representation in it of the State Government.
+		</div>
+		<div style="width: 360px;float: left;margin-left: 40px;padding-top: 20px;">
+		<font size="5"><b>&nbspOPTIONS</b><br></font>
+			<div id="divboxnb" style="line-height: 43px;">
+			<a href="#home" id="ahref" style="margin: 0px 0px 0px 0px;">Home</a>
+			<a href="#Alumni" id="ahref" style="margin: 0px 0px 0px 0px;">Alumni</a>
+			<a href="#about" id="ahref" style="margin: 0px 0px 0px 0px;padding-left: 39px;padding-right: 39px;">About Us</a>
+			<a href="#Gallery" id="ahref" style="margin: 0px 0px 0px 0px;">Gallery</a>
+			<a href="#collegemap" id="ahref" style="margin: 0px 0px 0px 0px;padding-left: 28px;padding-right: 27px;">College Map</a>
+			<a href="#contactus" id="ahref" style="margin: 0px 0px 0px 0px;padding-left: 35px;padding-right: 35px;">Contact Us</a>
+			</div>	
+		</div>
+		<div style="width: 410px;float: left;margin-left: 40px;padding-top: 20px;">
+			<font size="5"><b>ADDRESS</b><br></font>
+			Guru Nanak Dev Polytechnic College Gill Park,Gill Road, Ludhiana 141006 Punjab(India)
+			Society- Nankana Sahib Education Trust,Ludhiana.<br>
+			<br>E-mail:id@gmail.com
+			<br>Phone No. ####-#######,####-#######	
+		</div>
+		<hr align="center" width="1338px" style="float: left;border-color: #757575;border-width: 2px;margin: 5px 0px 0px 0px;">
+		<div style="float: left;">
+				<div style="width: 667px;float: left;line-height: 0px;padding-top: 15px">
+					<p>&nbspCopyright © 2018 - All Rights Reserved - <a href="index.html" style="text-decoration: none;color: white;">Collage Name</a></p>
+				</div>
+				<div style="width: 650px;float: right;text-align: right;line-height: 0px;padding-top: 15px">
+					<p>Designed By:-DGarg&nbsp</p>
+				</div>
+		</div>
+	</div>
+</div>
+</body>
+</html>
+
+
+
+
